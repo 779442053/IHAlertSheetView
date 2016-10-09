44ea8bba041efe20196a656c92b98cc9929ed0b0
Daredos <asiosldh@163.com>
2016-10-09 22:06:30 +0800

    优化一下代码

--- IHAlertSheetView/IHAlertSheetView/IHAlertSheetView/IHAlertSheetView.m
+++ IHAlertSheetView/IHAlertSheetView/IHAlertSheetView/IHAlertSheetView.m
@@ -9,7 +9,7 @@
 #import "IHAlertSheetView.h"
 
 #define kSystemVersionFloatValue      [[[UIDevice currentDevice] systemVersion] floatValue]
-#define kOS_VERSION_MIN_REQUIRED_7_0  10
+#define k__IPHONE_OS_VERSION_MIN_REQUIRED_7_0_  __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_8_0
 
 @interface IHAlertSheettAction ()
 @property (nonatomic, copy, readwrite) NSString *title;
@@ -31,13 +31,14 @@
 #pragma mark -
 
 @interface IHAlertSheetView ()
-#if kOS_VERSION_MIN_REQUIRED_7_0
+#if k__IPHONE_OS_VERSION_MIN_REQUIRED_7_0_
 <UIAlertViewDelegate, UIActionSheetDelegate>
 @property (nonatomic, strong) UIAlertView *alertView;
 @property (nonatomic, strong) UIActionSheet *actionSheet;
-@property (nonatomic, strong) NSMutableArray *blocks;
 #endif
+@property (nonatomic, strong) NSMutableArray *blocks;
 @property (nonatomic, strong) UIAlertController *alertController;
+
 
 @end
 
@@ -46,27 +47,31 @@
 #pragma mark -
 #pragma mark - init
 
+- (NSMutableArray *)blocks {
+    if (!_blocks) {
+        _blocks = [@[] mutableCopy];
+    }
+    return _blocks;
+}
 #pragma mark - 公有方法
 + (instancetype)alertWithTitle:(NSString *)title message:(NSString *)message {
     IHAlertSheetView *alertSheet = [IHAlertSheetView new];
-#if kOS_VERSION_MIN_REQUIRED_7_0
     if (kSystemVersionFloatValue < 8.0) {
+#if k__IPHONE_OS_VERSION_MIN_REQUIRED_7_0_
         alertSheet.alertView = [[UIAlertView alloc] initWithTitle:title message:message delegate:alertSheet cancelButtonTitle:nil otherButtonTitles:nil, nil];
+#endif
     }
     else {
         alertSheet.alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:1];
     }
-#else
-    alertSheet.alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:1];
-#endif
     return alertSheet;
 }
 
 + (instancetype)sheetWithTitle:(NSString *)title message:(NSString *)message cancelTitle:(NSString *)cancelTitle handler:(dispatch_block_t)handler {
     IHAlertSheetView *alertSheet = [IHAlertSheetView new];
-#if kOS_VERSION_MIN_REQUIRED_7_0
     alertSheet.alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:0];
     if (kSystemVersionFloatValue < 8.0) {
+#if k__IPHONE_OS_VERSION_MIN_REQUIRED_7_0_
         alertSheet.actionSheet = [[UIActionSheet alloc] initWithTitle:title delegate:alertSheet cancelButtonTitle:cancelTitle destructiveButtonTitle:nil otherButtonTitles:nil, nil];
         if (cancelTitle) {
             if (handler) {
@@ -75,6 +80,7 @@
                 [alertSheet.blocks addObject:^{}];
             }
         }
+#endif
     } else {
         if (cancelTitle) {
             UIAlertAction *alertAction = [UIAlertAction actionWithTitle:cancelTitle style:UIAlertActionStyleCancel handler:^(UIAlertAction *actio) {
@@ -85,24 +91,12 @@
             [alertSheet.alertController addAction:alertAction];
         }
     }
-#else
-    alertSheet.alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:0];
-    if (cancelTitle) {
-        UIAlertAction *alertAction = [UIAlertAction actionWithTitle:cancelTitle style:UIAlertActionStyleCancel handler:^(UIAlertAction *actio) {
-            if (handler) {
-                handler();
-            }
-        }];
-        [alertSheet.alertController addAction:alertAction];
-    }
-#endif
     return alertSheet;
 }
 
 - (void)addAction:(IHAlertSheettAction *)action {
-    
-#if kOS_VERSION_MIN_REQUIRED_7_0
     if (kSystemVersionFloatValue < 8.0) {
+#if k__IPHONE_OS_VERSION_MIN_REQUIRED_7_0_
         if (self.alertView) {
             [self.alertView addButtonWithTitle:action.title];
             [self.blocks addObject:action.handlerBlock];
@@ -110,6 +104,7 @@
             [self.actionSheet addButtonWithTitle:action.title];
             [self.blocks addObject:action.handlerBlock];
         }
+#endif
     }else{
         UIAlertAction *alertAction = [UIAlertAction actionWithTitle:action.title style:UIAlertActionStyleDefault handler:^(UIAlertAction *actio) {
             if (action.handlerBlock) {
@@ -118,14 +113,6 @@
         }];
         [self.alertController addAction:alertAction];
     }
-#else
-    UIAlertAction *alertAction = [UIAlertAction actionWithTitle:action.title style:UIAlertActionStyleDefault handler:^(UIAlertAction *actio) {
-        if (action.handlerBlock) {
-            action.handlerBlock();
-        }
-    }];
-    [self.alertController addAction:alertAction];
-#endif
 }
 
 - (void)addActions:(NSArray <IHAlertSheettAction *>*)actions {
@@ -135,19 +122,17 @@
 }
 
 - (void)show {
-#if kOS_VERSION_MIN_REQUIRED_7_0
     if (kSystemVersionFloatValue < 8.0) {
+#if k__IPHONE_OS_VERSION_MIN_REQUIRED_7_0_
         if (self.alertView) {
             [self.alertView show];
         }else{
             [self.actionSheet showInView:[self _topViewController].view];
         }
+#endif
     } else {
         [[self _topViewController] presentViewController:self.alertController animated:YES completion:nil];
     }
-#else
-    [[self _topViewController] presentViewController:self.alertController animated:YES completion:nil];
-#endif
 }
 
 - (void)addButtonWithTitle:(NSString *)title handler:(dispatch_block_t)handler {
@@ -163,14 +148,7 @@
         }]];
     }];
 }
-
-#if kOS_VERSION_MIN_REQUIRED_7_0
-- (NSMutableArray *)blocks {
-    if (!_blocks) {
-        _blocks = [@[] mutableCopy];
-    }
-    return _blocks;
-}
+#if k__IPHONE_OS_VERSION_MIN_REQUIRED_7_0_
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
     dispatch_block_t block = self.blocks[buttonIndex];
     block();
